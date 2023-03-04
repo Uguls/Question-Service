f3f8299ebf758903c02dbc0f6d7dafb373962d44
Uguls <chobozaex@gmail.com>
2023-03-04 13:32:45 +0900

    Entity 추가

---
+++ src/test/java/com/grade/webback/WebBackApplicationTests.java
@@ -0,0 +1,32 @@
+package com.grade.webback;
+
+import com.grade.webback.entity.Question;
+import com.grade.webback.repository.QuestionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+class WebBackApplicationTests {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void testJPpa() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreatDate(LocalDateTime.now());
+        this.questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreatDate(LocalDateTime.now());
+        this.questionRepository.save(q2);
+    }
+
+}
