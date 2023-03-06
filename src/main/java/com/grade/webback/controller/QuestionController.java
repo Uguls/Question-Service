6bb50ab5ed4542631ff168dfd45b5443e97fe8c8
Uguls <chobozaex@gmail.com>
2023-03-06 23:29:37 +0900

    질문 서비스 제작

---
+++ src/main/java/com/grade/webback/controller/QuestionController.java
@@ -0,0 +1,24 @@
+package com.grade.webback.controller;
+
+import com.grade.webback.repository.Question;
+import com.grade.webback.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("questionList", questionList);
+        return "question list";
+    }
+}
---
+++ src/main/java/com/grade/webback/service/QuestionService.java
@@ -0,0 +1,19 @@
+package com.grade.webback.service;
+
+import com.grade.webback.repository.Question;
+import com.grade.webback.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+}
