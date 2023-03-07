9cf0ad910c3fde7e8c28e699e61126b0e9ca03c5
Uguls <chobozaex@gmail.com>
2023-03-07 10:41:00 +0000

    스프링 작동 안되는 오류 해결

--- src/test/java/com/grade/webback/WebBackApplicationTests.java
+++ src/test/java/com/grade/webback/WebBackApplicationTests.java
@@ -1,25 +1,26 @@
-//package com.grade.webback;
-//
-//import com.grade.webback.entity.Question;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest
-//class WebBackApplicationTests {
-//
-//    @Autowired
-//    private QuestionRepository questionRepository;
-//
-//    @Test
-//    void testJPpa() {
-//        Optional<Question> oq = this.questionRepository.findById(1);
-//        if (oq.isPresent()) {
-//            Question q = oq.get();
-//            assertEquals("sbb가 무엇인가요?", q.getSubject());
-//        }
-//    }
-//}
+package com.grade.webback;
+
+import com.grade.webback.entity.Question;
+import com.grade.webback.repository.QuestionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class WebBackApplicationTests {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void testJPpa() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if (oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
+    }
+}
