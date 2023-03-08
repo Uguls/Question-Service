77ac9b5f8d8409aef58b9b0cd22feda52a0a83bc
Uguls <chobozaex@gmail.com>
2023-03-08 22:59:20 +0900

    질문 상세 목록 페이지 작성

---
+++ src/main/java/com/grade/webback/controller/AnswerController.java
@@ -0,0 +1,24 @@
+package com.grade.webback.controller;
+
+import com.grade.webback.entity.Question;
+import com.grade.webback.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+        Question question = this.questionService.getQuestion(id); // 답변을 저장한다.
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
--- src/test/java/com/grade/webback/WebBackApplicationTests.java
+++ src/test/java/com/grade/webback/WebBackApplicationTests.java
@@ -5,6 +5,8 @@ import com.grade.webback.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,11 +18,17 @@ class WebBackApplicationTests {
     private QuestionRepository questionRepository;
 
     @Test
-    void testJPpa() {
-        Optional<Question> oq = this.questionRepository.findById(1);
-        if (oq.isPresent()) {
-            Question q = oq.get();
-            assertEquals("sbb가 무엇인가요?", q.getSubject());
-        }
+    void testJpa() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreatDate(LocalDateTime.now());
+        this.questionRepository.save(q1);  // 첫번째 질문 저장
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreatDate(LocalDateTime.now());
+        this.questionRepository.save(q2);  // 두번째 질문 저장
     }
 }
