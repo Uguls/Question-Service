e51a0ab1412348d4af4d58e81ca837538be1e1e5
Uguls <chobozaex@gmail.com>
2023-03-09 23:26:41 +0900

    - 컨트롤러를 이용해 URL과 매핑되는 메소드를 관리한다. - 자바 JPA를 이용해 데이터베이스를 제어한다. - 게시판에 질문 목록과 질문 상세 기능을 만든다.

--- src/main/java/com/grade/webback/entity/Question.java
+++ src/main/java/com/grade/webback/entity/Question.java
@@ -21,7 +21,7 @@ public class Question {
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    private LocalDateTime creatDate;
+    private LocalDateTime createDate;
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
--- src/test/java/com/grade/webback/WebBackApplicationTests.java
+++ src/test/java/com/grade/webback/WebBackApplicationTests.java
@@ -7,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class WebBackApplicationTests {
@@ -22,13 +19,13 @@ class WebBackApplicationTests {
         Question q1 = new Question();
         q1.setSubject("sbb가 무엇인가요?");
         q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreatDate(LocalDateTime.now());
+        q1.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q1);  // 첫번째 질문 저장
 
         Question q2 = new Question();
         q2.setSubject("스프링부트 모델 질문입니다.");
         q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreatDate(LocalDateTime.now());
+        q2.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q2);  // 두번째 질문 저장
     }
 }
