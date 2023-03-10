2860210dfc01b7f7143eba2d9c3ac902b2669347
Uguls <chobozaex@gmail.com>
2023-03-10 20:51:09 +0900

    게시물 페이징

--- src/main/java/com/grade/webback/repository/QuestionRepository.java
+++ src/main/java/com/grade/webback/repository/QuestionRepository.java
@@ -1,14 +1,16 @@
 package com.grade.webback.repository;
 
 import com.grade.webback.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
-
     Question findBySubjectAndContent(String subject, String content);
-
     List<Question> findBySubjectLike(String subject);
+
+    Page<Question> findAll(Pageable pageable);
 }
