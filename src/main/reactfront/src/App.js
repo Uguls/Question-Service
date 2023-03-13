2bcc7d8bf7ed605140629c7bb971627aaeacee41
Uguls <chobozaex@gmail.com>
2023-03-14 00:18:37 +0900

    리액트랑 연결

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -1,22 +1,43 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import ListBoardComponent from './component/ListBoardComponent';
-import HeaderComponent from './component/HeaderComponent';
-import FooterComponent from './component/FooterComponent';
+// import './App.css';
+// import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+// import ListBoardComponent from './component/ListBoardComponent';
+// import HeaderComponent from './component/HeaderComponent';
+// import FooterComponent from './component/FooterComponent';
+//
+// function App() {
+//     return (
+//         <div>
+//             <Router>
+//                 <HeaderComponent/>
+//                 <div className="container">
+//                     <Routes>
+//                         <Route path = "/" exact={true} element = {ListBoardComponent}></Route>
+//                         <Route path = "/board" element = {ListBoardComponent}></Route>
+//                     </Routes>
+//                 </div>
+//                 <FooterComponent/>
+//             </Router>
+//         </div>
+//     );
+// }
+//
+// export default App;
+
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+    const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/hello')
+            .then(response => setHello(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return (
         <div>
-            <Router>
-                <HeaderComponent/>
-                <div className="container">
-                    <Routes>
-                        <Route path = "/" exact element = {ListBoardComponent}></Route>
-                        <Route path = "/board" element = {ListBoardComponent}></Route>
-                    </Routes>
-                </div>
-                <FooterComponent/>
-            </Router>
+            백엔드에서 가져온 데이터입니다 : {hello}
         </div>
     );
 }
